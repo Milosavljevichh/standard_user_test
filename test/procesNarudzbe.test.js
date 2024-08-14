00991aafab21e7082d497bcb8290d88849553a83
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2024-08-14 18:35:36 +0200

    added backpack sopping test

--- test/procesNarudzbe.test.js
+++ test/procesNarudzbe.test.js
@@ -3,13 +3,28 @@ const { Builder } = require('selenium-webdriver');
 const assert = require("assert")
 //Basic info
 const visitPage = "https://www.saucedemo.com/"
+const homePage = 'https://www.saucedemo.com/inventory.html'
+
 const loginUsername  = "standard_user"
 const loginPassword = "secret_sauce"
 
-//inputs
+const firstName = "Petar"
+const lastName = "Peric"
+const zipCode = "35000"
+
+//elements
 const usernameInput = {id: "user-name"}
 const passwordInput = {id: "password"}
 const logInBtn = {id: "login-button"}
+const backpackBtn = {id: "add-to-cart-sauce-labs-backpack"}
+const shoppingCart = {id: "shopping_cart_container"}
+const checkoutBtn = {id: "checkout"}
+const firstNameInput = {id: "first-name"}
+const lastNameInput = {id: "last-name"}
+const zipCodeInput = {id: "postal-code"}
+const finishBtn = {id: "finish"}
+const homeBtn = {id: "back-to-products"}
+const continueBtn = {id:"continue"}
 
 describe("Testovi za standard_usera", function(){
     this.timeout(10000)
@@ -28,13 +43,31 @@ describe("Testovi za standard_usera", function(){
 
     
     it("Proces narudzbine", async () => {
+        //logovanje
         await driver.findElement(usernameInput).sendKeys(loginUsername);
         await driver.findElement(passwordInput).sendKeys(loginPassword);
         await driver.findElement(logInBtn).click()
+        await driver.sleep(2000)
+
+        //dodavanje u korpu i checkout
+        await driver.findElement(backpackBtn).click()
+        await driver.findElement(shoppingCart).click()
+        await driver.findElement(checkoutBtn).click()
+
+        //popunjavanje informacija
+        await driver.findElement(firstNameInput).sendKeys(firstName)
+        await driver.findElement(lastNameInput).sendKeys(lastName)
+        await driver.findElement(zipCodeInput).sendKeys(zipCode)
+        await driver.findElement(continueBtn).click()
+
+        //zavrsni koraci
+        await driver.findElement(finishBtn).click()
+        await driver.findElement(homeBtn).click()
+
         // Dobijanje trenutnog URL-a
         const currentUrl = await driver.getCurrentUrl();
 
         // Provera da li je URL onaj koji očekujemo
-        assert.strictEqual(currentUrl, 'https://www.saucedemo.com/inventory.html', 'URL nije tačan');
+        assert.strictEqual(currentUrl, homePage, 'URL nije tačan');
     });
 })
